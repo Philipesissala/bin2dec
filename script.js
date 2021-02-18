60e910fd35e8b3ae3b827c9eae9d153f920d525c
Filipe Sissala <filipesissala14@gmail.com>
2021-02-18 21:39:57 +0100

    Refatorando

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const $input = document.querySelector("#entrada");
-const $output = document.querySelector("#saida");
+const $input = document.querySelector("#input");
+const $output = document.querySelector("#output");
 const $btn = document.querySelector("Button");
 
-function convertion(value) {
-    let binary = value.split("").reverse();
-    const results = binary.map((item, index) => {
-        return item * Math.pow(2, index);
-    });
-    const decimal = results.reduce((total, next) => {
-        return total + next;
-    });
-    $output.value = decimal;
+const converter = {
+    validate() {
+        const values = $input.value.split("");
+        const find = values.find(item => {
+            return item > 1
+        });
+        if (find > 1) {
+            alert("Erro, porfavor insira valores validos (0 ou 1)");
+            $input.value = "";
+            $output.value = "";
+        } else
+            converter.operation($input.value);
+    },
+    operation(value) {
+        let binary = value.split("").reverse();
+        const results = binary.map((item, index) => {
+            return item * Math.pow(2, index);
+        });
+        const decimal = results.reduce((total, next) => {
+            return total + next;
+        });
+        $output.value = decimal;
+    }
 }
-
 $btn.addEventListener("click", (event) => {
     event.preventDefault();
-    convertion($input.value);
+    converter.validate();
 });
